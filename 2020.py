8e8c630b801eaeea3f297beb3e77ccf30a5dfc00
Lhogeshwaran Purushothaman <lhogeshwaran.purushothaman@student.uts.edu.au>
2021-03-04 00:34:22 +1100

    B1::Success

--- 2020.py
+++ 2020.py
@@ -28,3 +28,32 @@ for i in range(1, t+1):
     cost = [int(s) for s in input().split(" ")]
     case = Case(for_sale, budget, cost)
     print(f'Case #{i}: {case.buyHouse()}')
+
+
+# ROUND B
+
+# Bike Tour (5pts, 7pts)
+
+class Case:
+    def __init__(self, checkpoints, heights):
+        self.checkpoints = checkpoints
+        self.heights = heights
+        
+    def countPeaks(self):
+        peaks = 0
+        
+        for idx, i in enumerate(self.heights):
+            if (idx>0) & (idx<self.checkpoints-1):
+                if (i>self.heights[idx-1]) and (i>self.heights[idx+1]):
+                    peaks += 1
+        
+        return peaks
+        
+
+t = int(input())
+for i in range(1, t+1):
+    checkpoints = int(input())
+    heights = [int(s) for s in input().split(" ")]
+    case = Case(checkpoints, heights)
+    print(f'Case #{i}: {case.countPeaks()}')
+    
