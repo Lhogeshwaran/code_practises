3d2c3f8c59b43d3b8185b68159235329f2ea94e4
Loki Purushothaman <lokiwork@Lhogeshwarans-MacBook-Pro.local>
2021-03-17 15:28:47 +1100

    Jumping on clouds :: Solved

--- hackerrank.py
+++ hackerrank.py
@@ -37,3 +37,43 @@ def countingValleys(steps, path):
 
 # Jumping on the Clouds
 # https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup&h_r=next-challenge&h_v=zen
+
+## My solution
+def jumpingOnClouds(c):
+    
+    track = 0
+    jumps = 0
+    
+    while track < len(c)-2:
+        if (c[track]==0) & (c[track+2]==0):
+            jumps += 1
+            track += 2
+        elif c[track]==0:
+            jumps += 1
+            track += 1
+        else:
+            track += 1
+     
+    if c[-2]==0:
+        jumps += 1
+
+    return jumps
+
+
+## Best solution
+def jumpingOnClouds(c):
+    if len(c) == 1: 
+        return 0
+    if len(c) == 2: 
+        return 0 if c[1]==1 else 1
+    if c[2]==1: 
+        return 1 + jumpingOnClouds(c[1:])
+    if c[2]==0:
+        return 1 + jumpingOnClouds(c[2:])
+
+## My first recursion function
+def getFact(n):
+
+    return n * getFact(n-1) if n > 0 else 1 
+
+getFact(10)
