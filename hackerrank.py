c8eded036ccb687eb54c6327e10143a7cf71bbcd
Loki Purushothaman <lokiwork@Lhogeshwarans-MacBook-Pro.local>
2021-03-17 17:37:01 +1100

    Repeated String :: Solved

--- hackerrank.py
+++ hackerrank.py
@@ -13,6 +13,7 @@ def sockMerchant(n, ar):
 # Counting Valleys
 # https://www.hackerrank.com/challenges/counting-valleys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
 
+
 def countingValleys(steps, path):
 
     altitude = 0
@@ -34,6 +35,7 @@ def countingValleys(steps, path):
             valleycount += 1
         
     return valleycount
+
 
 # Jumping on the Clouds
 # https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup&h_r=next-challenge&h_v=zen
@@ -62,6 +64,7 @@ def jumpingOnClouds(c):
 
 ## Best solution
 def jumpingOnClouds(c):
+
     if len(c) == 1: 
         return 0
     if len(c) == 2: 
@@ -71,9 +74,27 @@ def jumpingOnClouds(c):
     if c[2]==0:
         return 1 + jumpingOnClouds(c[2:])
 
+
 ## My first recursion function
 def getFact(n):
 
     return n * getFact(n-1) if n > 0 else 1 
 
 getFact(10)
+
+
+# Repeated String
+# https://www.hackerrank.com/challenges/repeated-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup&h_r=next-challenge&h_v=zen
+
+def repeatedString(s, n):
+
+    size = len(s)
+    reps = s.count('a')
+    occ = (divmod(n, size)[0]) * reps
+    
+    if divmod(n, size)[1] > 0:
+        sample = s[:divmod(n, size)[1]]
+        occ += sample.count('a')
+    
+    return occ
+
