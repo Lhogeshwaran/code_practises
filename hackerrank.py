e9ad8ad01549e870948358de3400d457a2d7b079
Loki Purushothaman <lokiwork@Lhogeshwarans-MacBook-Pro.local>
2021-03-18 20:51:08 +1100

    Hour glass sum :: Solved

--- hackerrank.py
+++ hackerrank.py
@@ -98,3 +98,18 @@ def repeatedString(s, n):
     
     return occ
 
+
+# Hour glass
+# https://www.hackerrank.com/challenges/2d-array/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+
+def hourglassSum(arr):
+    
+    sum = set()
+    
+    for i in range(len(arr)-2):
+        for j in range(len(arr)-2):
+            sum.add(arr[i][j] + arr[i][j+1] + arr[i][j+2] + 
+                    arr[i+1][j+1] + 
+                    arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2])
+            
+    return max(sum)
