223733d53d236ca0d5c1893a493d58b481f8522f
Loki Purushothaman <lokiwork@Lhogeshwarans-MacBook-Pro.local>
2021-03-29 19:37:45 +1100

    Removed file named l.py. Cosmetic changes to leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -13,8 +13,8 @@ class AddTwoNums:
         :param l1, l2: Two input lists
         :return: Sum of the two reversed lists as a reversed list
 
-        >>> AddTwoNums(l1, l2).addTwoNums()
-        [7, 0, 8]
+        >>> AddTwoNums([3, 6, 2, 5, 5], [4, 5, 3]).addTwoNums()
+        [7, 1, 6, 5, 5]
 
         """
 
@@ -22,18 +22,18 @@ class AddTwoNums:
         return [int(s) for s in tmp[::-1]]
 
 
-l1 = [2, 4, 3]
-l2 = [5, 6, 4]
+#l1 = [2, 4, 3]
+#l2 = [5, 6, 4]
 
-l1 = [4, 5, 2, 1]
-l2 = [3, 6, 2, 5, 5]
+#l1 = [4, 5, 2, 1]
+#l2 = [3, 6, 2, 5, 5]
 
 l1 = [3, 6, 2, 5, 5]
 l2 = [4, 5, 3]
 
-l1 = [9,9,9,9,9,9,9]
-l2 = [9,9,9,9]
+#l1 = [9,9,9,9,9,9,9]
+#l2 = [9,9,9,9]
 
 AddTwoNums(l1, l2).addTwoNums()
 
-# doctest.testmod()
+doctest.testmod()
